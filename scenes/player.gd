a3c380bca70cf356ae0a0b1adfc7234f140800e3
Apiseg Chainarong <apiseg.chainarong@outlook.com>
2023-12-28 16:54:12 +0100

    changing model depending on standing or crouching

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,6 +1,12 @@
 extends CharacterBody3D
 
 @onready var head = $Head
+@onready var standing_mesh = $StandingMesh
+@onready var standing_collision_shape = $StandingCollisionShape
+
+@onready var crouching_mesh = $CrouchingMesh
+@onready var crouching_collision_shape = $CrouchingCollisionShape
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var GRAVITY = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -37,16 +43,39 @@ func handle_gravity(delta: float) -> void:
 		velocity.y -= GRAVITY * delta
 
 func handle_character_properties(delta: float) -> void:
-	head.position.y = lerp(head.position.y, 0.6, delta * LERP_SPEED)
+
 	
 	if Input.is_action_pressed("sprint"):
 		current_speed = SPRINTING_SPEED
+		enable_standing_model()
+		set_default_head_position(delta)
+		
 		
 	elif Input.is_action_pressed("crouch"):
-		head.position.y = lerp(head.position.y, 0.6 - CROUCHING_DEPTH, delta * LERP_SPEED)
 		current_speed = CROUCHING_SPEED
+		enable_crouching_model()
+		head.position.y = lerp(head.position.y, 0.6 - CROUCHING_DEPTH, delta * LERP_SPEED)
+		
+		
 	else :
 		current_speed = WALKING_SPEED
+		enable_standing_model()
+		set_default_head_position(delta)
+		
+func set_default_head_position(delta: float) -> void:
+	head.position.y = lerp(head.position.y, 0.6, delta * LERP_SPEED)
+
+func enable_crouching_model() -> void:
+	standing_collision_shape.disabled = true
+	standing_mesh.visible = false
+	crouching_collision_shape.disabled = false
+	crouching_mesh.visible = true
+	
+func enable_standing_model() -> void:
+	standing_collision_shape.disabled = false
+	standing_mesh.visible = true
+	crouching_collision_shape.disabled = true
+	crouching_mesh.visible = false
 
 func handle_character_movement(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
